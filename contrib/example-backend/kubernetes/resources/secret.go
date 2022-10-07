a55035773a0b3b063ad1411f02708f0e816d4c8f
Moath Qasim <moad.qassem@gmail.com>
2022-10-08 00:15:27 +0200

    add get secret method to fetch existing secret

--- contrib/example-backend/kubernetes/resources/secret.go
+++ contrib/example-backend/kubernetes/resources/secret.go
@@ -18,28 +18,31 @@ package resources
 
 import (
 	"context"
-	"k8s.io/apimachinery/pkg/api/errors"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	k8s "k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/kubernetes"
 )
 
-func CreateSASecret(ctx context.Context, client *k8s.Clientset, ns string) (*corev1.Secret, error) {
-	secret := &corev1.Secret{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      ClusterAdminName,
-			Namespace: ns,
-			Annotations: map[string]string{
-				ServiceAccountTokenAnnotation: ClusterAdminName,
-			},
-		},
-		Data: nil,
-		Type: ServiceAccountTokenType,
-	}
+func CreateSASecret(ctx context.Context, client kubernetes.Interface, ns string) (*corev1.Secret, error) {
+	secret, err := client.CoreV1().Secrets(ns).Get(ctx, ClusterAdminName, metav1.GetOptions{})
+	if err != nil {
+		if errors.IsNotFound(err) {
+			secret = &corev1.Secret{
+				ObjectMeta: metav1.ObjectMeta{
+					Name:      ClusterAdminName,
+					Namespace: ns,
+					Annotations: map[string]string{
+						ServiceAccountTokenAnnotation: ClusterAdminName,
+					},
+				},
+				Type: ServiceAccountTokenType,
+			}
 
-	secret, err := client.CoreV1().Secrets(ns).Create(ctx, secret, metav1.CreateOptions{})
-	if err != nil && !errors.IsAlreadyExists(err) {
+			return client.CoreV1().Secrets(ns).Create(ctx, secret, metav1.CreateOptions{})
+		}
+
 		return nil, err
 	}
 

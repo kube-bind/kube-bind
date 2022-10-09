72a837dd8aa9357fb4bcaf0fea797509859e0ed1
Dr. Stefan Schimanski <stefan.schimanski@gmail.com>
2022-10-09 13:51:50 +0200

    konnector: base plumbing

---
+++ pkg/konnector/controllers/namespacedeletion/namespacedeletion_controller.go
@@ -0,0 +1,223 @@
+/*
+Copyright 2022 The kube bind Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package namespacedeletion
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	coreinformers "k8s.io/client-go/informers/core/v1"
+	kubernetesclient "k8s.io/client-go/kubernetes"
+	corelisters "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+
+	kubebindv1alpha1 "github.com/kube-bind/kube-bind/pkg/apis/kubebind/v1alpha1"
+	bindclient "github.com/kube-bind/kube-bind/pkg/client/clientset/versioned"
+	bindinformers "github.com/kube-bind/kube-bind/pkg/client/informers/externalversions/kubebind/v1alpha1"
+	bindlisters "github.com/kube-bind/kube-bind/pkg/client/listers/kubebind/v1alpha1"
+)
+
+const (
+	controllerName = "kube-bind-konnector-namespacedeletion"
+)
+
+// NewController returns a new controller deleting old ServiceNamespaces.
+func NewController(
+	config *rest.Config,
+	serviceNamespaceInformer bindinformers.ServiceNamespaceInformer,
+	namespaceInformer coreinformers.NamespaceInformer,
+) (*controller, error) {
+	queue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), controllerName)
+
+	logger := klog.Background().WithValues("controller", controllerName)
+
+	config = rest.CopyConfig(config)
+	config = rest.AddUserAgent(config, controllerName)
+
+	bindClient, err := bindclient.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+	kubeClient, err := kubernetesclient.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &controller{
+		queue: queue,
+
+		bindClient: bindClient,
+		kubeClient: kubeClient,
+
+		serviceNamespaceLister:  serviceNamespaceInformer.Lister(),
+		serviceNamespaceIndexer: serviceNamespaceInformer.Informer().GetIndexer(),
+
+		namespaceLister:  namespaceInformer.Lister(),
+		namespaceIndexer: namespaceInformer.Informer().GetIndexer(),
+
+		getNamespace: namespaceInformer.Lister().Get,
+
+		getServiceNamespace: func(ns, name string) (*kubebindv1alpha1.ServiceNamespace, error) {
+			return serviceNamespaceInformer.Lister().ServiceNamespaces(ns).Get(name)
+		},
+		deleteServiceNamespace: func(ctx context.Context, ns, name string) error {
+			return bindClient.KubeBindV1alpha1().ServiceNamespaces(ns).Delete(ctx, name, metav1.DeleteOptions{})
+		},
+	}
+
+	namespaceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			c.enqueueNamespace(logger, obj)
+		},
+		UpdateFunc: func(_, newObj interface{}) {
+			c.enqueueNamespace(logger, newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			c.enqueueNamespace(logger, obj)
+		},
+	})
+
+	serviceNamespaceInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			c.enqueueServiceNamespace(logger, obj)
+		},
+		UpdateFunc: func(_, newObj interface{}) {
+			c.enqueueServiceNamespace(logger, newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			c.enqueueServiceNamespace(logger, obj)
+		},
+	})
+
+	return c, nil
+}
+
+// controller reconciles ServiceNamespaces by creating a Namespace for each, and deleting it if
+// the ServiceNamespace is deleted.
+type controller struct {
+	queue workqueue.RateLimitingInterface
+
+	bindClient bindclient.Interface
+	kubeClient kubernetesclient.Interface
+
+	namespaceLister  corelisters.NamespaceLister
+	namespaceIndexer cache.Indexer
+
+	serviceNamespaceLister  bindlisters.ServiceNamespaceLister
+	serviceNamespaceIndexer cache.Indexer
+
+	getNamespace           func(name string) (*corev1.Namespace, error)
+	getServiceNamespace    func(ns, name string) (*kubebindv1alpha1.ServiceNamespace, error)
+	deleteServiceNamespace func(ctx context.Context, ns, name string) error
+}
+
+func (c *controller) enqueueServiceNamespace(logger klog.Logger, obj interface{}) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+
+	logger.V(2).Info("queueing ServiceNamespace", "key", key)
+	c.queue.Add(key)
+}
+
+func (c *controller) enqueueNamespace(logger klog.Logger, obj interface{}) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+
+	logger.V(2).Info("queueing ServiceNamespace", "key", key, "reason", "Namespace", "NamespaceKey", key)
+	c.queue.Add(key)
+}
+
+// Start starts the controller, which stops when ctx.Done() is closed.
+func (c *controller) Start(ctx context.Context, numThreads int) {
+	defer runtime.HandleCrash()
+	defer c.queue.ShutDown()
+
+	logger := klog.FromContext(ctx).WithValues("controller", controllerName)
+
+	logger.Info("Starting controller")
+	defer logger.Info("Shutting down controller")
+
+	for i := 0; i < numThreads; i++ {
+		go wait.UntilWithContext(ctx, c.startWorker, time.Second)
+	}
+
+	<-ctx.Done()
+}
+
+func (c *controller) startWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
+
+func (c *controller) processNextWorkItem(ctx context.Context) bool {
+	// Wait until there is a new item in the working queue
+	k, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	key := k.(string)
+
+	logger := klog.FromContext(ctx).WithValues("key", key)
+	ctx = klog.NewContext(ctx, logger)
+	logger.Info("processing key")
+
+	// No matter what, tell the queue we're done with this key, to unblock
+	// other workers.
+	defer c.queue.Done(key)
+
+	if err := c.process(ctx, key); err != nil {
+		runtime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", controllerName, key, err))
+		c.queue.AddRateLimited(key)
+		return true
+	}
+	c.queue.Forget(key)
+	return true
+}
+
+func (c *controller) process(ctx context.Context, key string) error {
+	snsNamespace, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(err)
+		return nil // we cannot do anything
+	}
+
+	if _, err := c.getNamespace(name); err != nil && !errors.IsNotFound(err) {
+		return err
+	} else if errors.IsNotFound(err) {
+		if err := c.deleteServiceNamespace(ctx, snsNamespace, name); err != nil && !errors.IsNotFound(err) {
+			return err
+		}
+		return nil
+	}
+
+	return nil
+}
---
+++ pkg/konnector/konnector_controller.go
@@ -0,0 +1,258 @@
+/*
+Copyright 2021 The kube bind Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package konnector
+
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	utilerrors "k8s.io/apimachinery/pkg/util/errors"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/apimachinery/pkg/util/wait"
+	coreinformers "k8s.io/client-go/informers/core/v1"
+	corelisters "k8s.io/client-go/listers/core/v1"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
+	"k8s.io/client-go/util/workqueue"
+	"k8s.io/klog/v2"
+
+	kubebindv1alpha1 "github.com/kube-bind/kube-bind/pkg/apis/kubebind/v1alpha1"
+	bindclient "github.com/kube-bind/kube-bind/pkg/client/clientset/versioned"
+	bindinformers "github.com/kube-bind/kube-bind/pkg/client/informers/externalversions/kubebind/v1alpha1"
+	bindlisters "github.com/kube-bind/kube-bind/pkg/client/listers/kubebind/v1alpha1"
+	"github.com/kube-bind/kube-bind/pkg/committer"
+)
+
+const (
+	controllerName = "kube-bind-konnector"
+)
+
+// New returns a konnector controller.
+func New(
+	config *rest.Config,
+	serviceBindingInformer bindinformers.ServiceBindingInformer,
+	secretInformer coreinformers.SecretInformer,
+) (*controller, error) {
+	queue := workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), controllerName)
+
+	logger := klog.Background().WithValues("controller", controllerName)
+
+	config = rest.CopyConfig(config)
+	config = rest.AddUserAgent(config, controllerName)
+
+	bindClient, err := bindclient.NewForConfig(config)
+	if err != nil {
+		return nil, err
+	}
+
+	c := &controller{
+		queue: queue,
+
+		bindClient: bindClient,
+
+		serviceBindingLister:  serviceBindingInformer.Lister(),
+		serviceBindingIndexer: serviceBindingInformer.Informer().GetIndexer(),
+
+		secretLister:  secretInformer.Lister(),
+		secretIndexer: secretInformer.Informer().GetIndexer(),
+	}
+
+	// nolint:errcheck
+	serviceBindingInformer.Informer().AddIndexers(cache.Indexers{
+		ByKubeconfigSecret: IndexByKubeconfigSecret,
+	})
+
+	serviceBindingInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			c.enqueueServiceBinding(logger, obj)
+		},
+		UpdateFunc: func(_, newObj interface{}) {
+			c.enqueueServiceBinding(logger, newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			c.enqueueServiceBinding(logger, obj)
+		},
+	})
+
+	secretInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			c.enqueueSecret(logger, obj)
+		},
+		UpdateFunc: func(_, newObj interface{}) {
+			c.enqueueSecret(logger, newObj)
+		},
+		DeleteFunc: func(obj interface{}) {
+			c.enqueueSecret(logger, obj)
+		},
+	})
+	return c, nil
+}
+
+type Resource = committer.Resource[*kubebindv1alpha1.ServiceBindingSpec, *kubebindv1alpha1.ServiceBindingStatus]
+type CommitFunc = func(context.Context, *Resource, *Resource) error
+
+// controller is the top-level controller watching ServiceBindings and
+// service provider credentials, and then starts ServiceBinding controllers
+// dynamically.
+type controller struct {
+	queue workqueue.RateLimitingInterface
+
+	bindClient bindclient.Interface
+
+	serviceBindingLister  bindlisters.ServiceBindingLister
+	serviceBindingIndexer cache.Indexer
+
+	secretLister  corelisters.SecretLister
+	secretIndexer cache.Indexer
+
+	// Store the kubeconfig from the secrets. Every time a kubeconfig
+	// changes, some controllers have to be restarted to pick up the
+	// new credentials.
+	// nolint:unused
+	bindingKubeconfigLock sync.Mutex
+	// nolint:unused
+	bindingKubeconfigs map[string][]byte // by binding name
+
+	commit CommitFunc
+}
+
+func (c *controller) enqueueServiceBinding(logger klog.Logger, obj interface{}) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+
+	logger.V(2).Info("queueing ServiceBinding", "key", key)
+	c.queue.Add(key)
+}
+
+func (c *controller) enqueueSecret(logger klog.Logger, obj interface{}) {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	ns, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+
+	bindings, err := c.serviceBindingIndexer.ByIndex(ByKubeconfigSecret, fmt.Sprintf("%s/%s", ns, name))
+	if err != nil {
+		runtime.HandleError(err)
+		return
+	}
+	if len(bindings) == 0 {
+		return
+	}
+
+	for _, obj := range bindings {
+		bindingKey, err := cache.MetaNamespaceKeyFunc(obj)
+		if err != nil {
+			runtime.HandleError(err)
+			continue
+		}
+		logger.V(2).Info("queueing ServiceBinding", "key", bindingKey, "reason", "Secret", "SecretKey", key)
+		c.queue.Add(bindingKey)
+	}
+}
+
+// Start starts the konnector. It does block.
+func (k *controller) Start(ctx context.Context, numThreads int) {
+	defer runtime.HandleCrash()
+	defer k.queue.ShutDown()
+
+	logger := klog.FromContext(ctx).WithValues("controller", controllerName)
+
+	logger.Info("Starting controller")
+	defer logger.Info("Shutting down controller")
+
+	for i := 0; i < numThreads; i++ {
+		go wait.UntilWithContext(ctx, k.startWorker, time.Second)
+	}
+}
+
+func (c *controller) startWorker(ctx context.Context) {
+	for c.processNextWorkItem(ctx) {
+	}
+}
+
+func (c *controller) processNextWorkItem(ctx context.Context) bool {
+	// Wait until there is a new item in the working queue
+	k, quit := c.queue.Get()
+	if quit {
+		return false
+	}
+	key := k.(string)
+
+	logger := klog.FromContext(ctx).WithValues("key", key)
+	ctx = klog.NewContext(ctx, logger)
+	logger.Info("processing key")
+
+	// No matter what, tell the queue we're done with this key, to unblock
+	// other workers.
+	defer c.queue.Done(key)
+
+	if err := c.process(ctx, key); err != nil {
+		runtime.HandleError(fmt.Errorf("%q controller failed to sync %q, err: %w", controllerName, key, err))
+		c.queue.AddRateLimited(key)
+		return true
+	}
+	c.queue.Forget(key)
+	return true
+}
+
+func (c *controller) process(ctx context.Context, key string) error {
+	_, name, err := cache.SplitMetaNamespaceKey(key)
+	if err != nil {
+		runtime.HandleError(err)
+		return nil // we cannot do anything
+	}
+
+	obj, err := c.serviceBindingLister.Get(name)
+	if err != nil && !errors.IsNotFound(err) {
+		return err
+	} else if errors.IsNotFound(err) {
+		// update remote condition
+		return nil
+	}
+
+	old := obj
+	obj = obj.DeepCopy()
+
+	var errs []error
+	if err := c.reconcile(ctx, obj); err != nil {
+		errs = append(errs, err)
+	}
+
+	// Regardless of whether reconcile returned an error or not, always try to patch status if needed. Return the
+	// reconciliation error at the end.
+
+	// If the object being reconciled changed as a result, update it.
+	oldResource := &Resource{ObjectMeta: old.ObjectMeta, Spec: &old.Spec, Status: &old.Status}
+	newResource := &Resource{ObjectMeta: obj.ObjectMeta, Spec: &obj.Spec, Status: &obj.Status}
+	if err := c.commit(ctx, oldResource, newResource); err != nil {
+		errs = append(errs, err)
+	}
+
+	return utilerrors.NewAggregate(errs)
+}

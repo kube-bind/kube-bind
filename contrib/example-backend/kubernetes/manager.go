c8e13cddb6cdae0fcd8abeb8f003816c672a6bc7
Moath Qasim <moad.qassem@gmail.com>
2022-10-10 10:27:43 +0200

    add SaveUserData and FetchUserData for the example backend
    Signed-off-by: Moath Qasim <moad.qassem@gmail.com>

--- contrib/example-backend/kubernetes/manager.go
+++ contrib/example-backend/kubernetes/manager.go
@@ -18,7 +18,9 @@ package kubernetes
 
 import (
 	"context"
-
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	kubeclient "k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 
@@ -40,6 +42,11 @@ func NewKubernetesManager(config *rest.Config, clusterName, ns string) (*Manager
 		return nil, err
 	}
 
+	err = kuberesources.CreateNamespace(context.TODO(), ns, kubeClient)
+	if err != nil {
+		return nil, err
+	}
+
 	bindClient, err := bindclient.NewForConfig(config)
 	if err != nil {
 		return nil, err
@@ -54,12 +61,39 @@ func NewKubernetesManager(config *rest.Config, clusterName, ns string) (*Manager
 	}, nil
 }
 
-func (m *Manager) HandleResources(ctx context.Context) ([]byte, error) {
-	err := kuberesources.CreateNamespace(ctx, m.namespace, m.kubeClient)
+func (m *Manager) SaveUserData(ctx context.Context, auth *kuberesources.AuthCode) error {
+	secret, err := m.kubeClient.CoreV1().Secrets(m.namespace).Get(ctx, kuberesources.SessionIDs, v1.GetOptions{})
 	if err != nil {
-		return nil, err
+		if errors.IsNotFound(err) {
+			secret = &corev1.Secret{
+				ObjectMeta: v1.ObjectMeta{
+					Name:      kuberesources.SessionIDs,
+					Namespace: m.namespace,
+				},
+				Data: map[string][]byte{
+					auth.SessionID: []byte(auth.RedirectURL),
+				},
+			}
+
+			_, err := m.kubeClient.CoreV1().Secrets(m.namespace).Create(ctx, secret, v1.CreateOptions{})
+			if err == nil {
+				return nil
+			}
+		}
+
+		return err
 	}
 
+	secret.Data[auth.SessionID] = []byte(auth.RedirectURL)
+	_, err = m.kubeClient.CoreV1().Secrets(m.namespace).Update(ctx, secret, v1.UpdateOptions{})
+	return err
+}
+
+func (m *Manager) FetchUserData(ctx context.Context) (*corev1.Secret, error) {
+	return m.kubeClient.CoreV1().Secrets(m.namespace).Get(ctx, kuberesources.SessionIDs, v1.GetOptions{})
+}
+
+func (m *Manager) HandleResources(ctx context.Context) ([]byte, error) {
 	saSecret, err := kuberesources.CreateSASecret(ctx, m.kubeClient, m.namespace)
 	if err != nil {
 		return nil, err

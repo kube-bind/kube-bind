4e46da2cba947ddbeae459870fb720f0b267279a
Nelo-T. Wallus <n.wallus@sap.com>
2025-10-01 15:09:03 +0200

    Add kcp e2e test
    Signed-off-by: Nelo-T. Wallus <red.brush9525@fastmail.com>
    Signed-off-by: Nelo-T. Wallus <n.wallus@sap.com>

---
+++ contrib/kcp/test/e2e/kcp.go
@@ -0,0 +1,154 @@
+/*
+Copyright 2025 The Kube Bind Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package e2e
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
+
+	kcpapisv1alpha2 "github.com/kcp-dev/kcp/sdk/apis/apis/v1alpha2"
+	kcpclientset "github.com/kcp-dev/kcp/sdk/client/clientset/versioned/cluster"
+	kcptestinghelpers "github.com/kcp-dev/kcp/sdk/testing/helpers"
+	kcptestingserver "github.com/kcp-dev/kcp/sdk/testing/server"
+	"github.com/kcp-dev/logicalcluster/v3"
+	"github.com/spf13/pflag"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/rest"
+
+	"github.com/kube-bind/kube-bind/contrib/kcp/bootstrap"
+	"github.com/kube-bind/kube-bind/contrib/kcp/bootstrap/options"
+	"github.com/kube-bind/kube-bind/test/e2e/framework"
+)
+
+func wsConfig(t testing.TB, server kcptestingserver.RunningServer, workspace logicalcluster.Path) (*rest.Config, string) {
+	cfg := server.BaseConfig(t)
+	cfg.Host += "/clusters/" + workspace.String()
+
+	kubeconfig := framework.WriteKubeconfig(t,
+		framework.RestToKubeconfig(cfg, "default"),
+		workspace.Base()+".kubeconfig",
+	)
+
+	return cfg, kubeconfig
+}
+
+func generateApiBinding(t testing.TB, path logicalcluster.Path, name, exportName string, acceptPermissionClaims ...string) *kcpapisv1alpha2.APIBinding {
+	t.Helper()
+
+	apiBinding := &kcpapisv1alpha2.APIBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: name,
+		},
+		Spec: kcpapisv1alpha2.APIBindingSpec{
+			Reference: kcpapisv1alpha2.BindingReference{
+				Export: &kcpapisv1alpha2.ExportBindingReference{
+					Path: path.String(),
+					Name: exportName,
+				},
+			},
+		},
+	}
+
+	for _, claim := range acceptPermissionClaims {
+		split := strings.SplitN(claim, ".", 2)
+		require.Len(t, split, 2, "invalid permission claim: %s, must be in the form <resource>.<group>", claim)
+
+		resource, group := split[0], split[1]
+		require.NotEmpty(t, group, "invalid permission claim: %s, group cannot be empty", claim)
+		require.NotEmpty(t, resource, "invalid permission claim: %s, resource cannot be empty", claim)
+
+		if group == "core" {
+			group = ""
+		}
+
+		apiBinding.Spec.PermissionClaims = append(
+			apiBinding.Spec.PermissionClaims,
+			kcpapisv1alpha2.AcceptablePermissionClaim{
+				State: kcpapisv1alpha2.ClaimAccepted,
+				ScopedPermissionClaim: kcpapisv1alpha2.ScopedPermissionClaim{
+					Selector: kcpapisv1alpha2.PermissionClaimSelector{
+						MatchAll: true,
+					},
+					PermissionClaim: kcpapisv1alpha2.PermissionClaim{
+						GroupResource: kcpapisv1alpha2.GroupResource{
+							Group:    group,
+							Resource: resource,
+						},
+						Verbs: []string{"*"},
+					},
+				},
+			},
+		)
+	}
+
+	return apiBinding
+}
+
+func createApiBinding(t testing.TB, client *kcpclientset.ClusterClientset, path logicalcluster.Path, apiBinding *kcpapisv1alpha2.APIBinding) *kcpapisv1alpha2.APIBinding {
+	t.Helper()
+
+	_, err := client.Cluster(path).
+		ApisV1alpha2().
+		APIBindings().
+		Create(t.Context(), apiBinding, metav1.CreateOptions{})
+	require.NoError(t, err, "Error creating APIBinding")
+
+	var inCluster *kcpapisv1alpha2.APIBinding
+	kcptestinghelpers.Eventually(t, func() (bool, string) {
+		var err error
+		inCluster, err = client.
+			Cluster(path).
+			ApisV1alpha2().
+			APIBindings().
+			Get(t.Context(), apiBinding.Name, metav1.GetOptions{})
+		if err != nil {
+			return false, fmt.Sprintf("Error getting APIBinding: %v", err)
+		}
+		return inCluster.Status.Phase == kcpapisv1alpha2.APIBindingPhaseBound, fmt.Sprintf("APIBinding not ready: %#v", inCluster.Status)
+	}, wait.ForeverTestTimeout, time.Millisecond*100)
+	return inCluster
+}
+
+func bootstrapKCP(t testing.TB, server kcptestingserver.RunningServer) {
+	t.Helper()
+	t.Log("Bootstrapping KCP")
+
+	cfg := server.BaseConfig(t)
+	cfg.Host += "/clusters/root"
+	adminApiCfg := framework.RestToKubeconfig(cfg, "default")
+	adminKubeconfig := framework.WriteKubeconfig(t, adminApiCfg, "admin.kubeconfig")
+
+	fs := pflag.NewFlagSet("kcp-bootstrap", pflag.ContinueOnError)
+	options := options.NewOptions()
+	options.AddFlags(fs)
+
+	options.KCPKubeConfig = adminKubeconfig
+
+	completed, err := options.Complete()
+	require.NoError(t, err)
+
+	config, err := bootstrap.NewConfig(completed)
+	require.NoError(t, err)
+
+	bootstrapper, err := bootstrap.NewServer(t.Context(), config)
+	require.NoError(t, err)
+	require.NoError(t, bootstrapper.Start(t.Context()))
+}
--- contrib/kcp/test/e2e/kcp_test.go
+++ contrib/kcp/test/e2e/kcp_test.go
@@ -16,8 +16,231 @@ limitations under the License.
 
 package e2e
 
-import "testing"
+import (
+	"fmt"
+	"strings"
+	"testing"
+	"time"
 
-func TestSkip(t *testing.T) {
-	t.Skip("noop so test-e2e-contribs doesn't fail")
+	kcpclientset "github.com/kcp-dev/kcp/sdk/client/clientset/versioned/cluster"
+	kcptesting "github.com/kcp-dev/kcp/sdk/testing"
+	kcptestinghelpers "github.com/kcp-dev/kcp/sdk/testing/helpers"
+	"github.com/kcp-dev/logicalcluster/v3"
+	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/util/wait"
+	"k8s.io/client-go/dynamic"
+	"k8s.io/client-go/rest"
+
+	kubebindv1alpha2 "github.com/kube-bind/kube-bind/sdk/apis/kubebind/v1alpha2"
+	"github.com/kube-bind/kube-bind/test/e2e/framework"
+)
+
+func TestKCPClusterScope(t *testing.T) {
+	t.Parallel()
+	testKcpIntegration(t, kubebindv1alpha2.ClusterScope)
+}
+
+func TestKCPNamespacedScope(t *testing.T) {
+	t.Parallel()
+	testKcpIntegration(t, kubebindv1alpha2.NamespacedScope)
+}
+
+func testKcpIntegration(t *testing.T, scope kubebindv1alpha2.InformerScope) {
+	t.Helper()
+	t.Logf("Testing KCP integration with informer scope %s, tempdir: %s", scope, t.TempDir())
+
+	// dex
+	framework.StartDex(t)
+
+	// kcp bootstrap
+	server := kcptesting.PrivateKcpServer(t)
+	bootstrapKCP(t, server)
+
+	// consumer
+	t.Log("Create consumer workspace")
+	consumerWsPath, _ := kcptesting.NewWorkspaceFixture(t, server, logicalcluster.NewPath("root"), kcptesting.WithName("consumer"))
+
+	t.Log("Create a consumer kubeconfig")
+	consumerCfg, consumerKubeconfig := wsConfig(t, server, consumerWsPath)
+
+	t.Log("Start konnector for consumer workspace")
+	framework.StartKonnector(t, consumerCfg, "--kubeconfig="+consumerKubeconfig)
+
+	// backend
+	backendAddr := bootstrapBackend(t, server, scope)
+
+	// provider
+	t.Log("Create provider workspace")
+	providerWsPath, _ := kcptesting.NewWorkspaceFixture(t, server, logicalcluster.NewPath("root"), kcptesting.WithName("provider"))
+
+	t.Log("Create a provider kubeconfig")
+	providerCfg, _ := wsConfig(t, server, providerWsPath)
+
+	cfg := server.BaseConfig(t)
+	kcpClusterClient, err := kcpclientset.NewForConfig(cfg)
+	require.NoError(t, err, "failed to create kcp client")
+
+	t.Log("Bind kube-bind.io APIExport to provider workspace")
+	createApiBinding(t,
+		kcpClusterClient,
+		providerWsPath,
+		generateApiBinding(t, logicalcluster.NewPath("root").Join("kube-bind"), "kube-bind.io", "kube-bind.io",
+			"clusterrolebindings.rbac.authorization.k8s.io",
+			"clusterroles.rbac.authorization.k8s.io",
+			"customresourcedefinitions.apiextensions.k8s.io",
+			"serviceaccounts.core",
+			"configmaps.core",
+			"secrets.core",
+			"namespaces.core",
+			"roles.rbac.authorization.k8s.io",
+			"rolebindings.rbac.authorization.k8s.io",
+			"apiresourceschemas.apis.kcp.io",
+		),
+	)
+
+	t.Log("Applying example APIExport and APIResourceSchemas to provider workspace")
+	framework.ApplyFiles(t,
+		providerCfg,
+		"../../deploy/examples/apiexport.yaml",
+		"../../deploy/examples/apiresourceschema-cowboys.yaml",  // namespaced
+		"../../deploy/examples/apiresourceschema-sheriffs.yaml", // cluster scoped
+	)
+
+	t.Log("Bind the APIExport locally")
+	createApiBinding(t,
+		kcpClusterClient,
+		providerWsPath,
+		generateApiBinding(t, providerWsPath, "cowboys-stable", "cowboys-stable"),
+	)
+
+	t.Log("Get logical cluster of provider workspace")
+	providerCluster, err := kcpClusterClient.Cluster(providerWsPath).CoreV1alpha1().LogicalClusters().Get(t.Context(), "cluster", metav1.GetOptions{})
+	require.NoError(t, err)
+	require.NotEmpty(t, providerCluster.Status.URL, "provider cluster URL is empty")
+
+	// .Status.URL should have the form: <scheme>://(....)/clusters/<cluster-id>, the <cluster-id> is what is needed for kube-bind
+	// The URL overall is irrelevant, so split on `/` and validate that
+	// at least the tail end is /cluster/<cluster-id as expected
+	providerClusterSplit := strings.Split(providerCluster.Status.URL, "/")
+	// Validate the length of the split to prevent panics, need at least
+	// two segments, should be something like
+	// ["https:", "", "....", "clusters", "<cluster-id>"]
+	require.GreaterOrEqual(t, len(providerClusterSplit), 2, "Unexpected URL format: %s", providerCluster.Status.URL)
+	// Validate the second last segment is "clusters" as a sanity check
+	require.Equal(t, "clusters", providerClusterSplit[len(providerClusterSplit)-2], "Unexpected URL format: %s", providerCluster.Status.URL)
+	// Can assume that the last entry is now the cluster-id, grab it and
+	// sanity check that it's not empty
+	providerClusterID := providerClusterSplit[len(providerClusterSplit)-1]
+	require.NotEmpty(t, providerClusterID, "Retreived cluster id is empty, source URL: %s", providerCluster.Status.URL)
+
+	// kube-bind process
+	t.Log("Perform binding process with browser")
+	var kind, resource string
+	switch scope {
+	case kubebindv1alpha2.ClusterScope:
+		kind = "Sheriff"
+		resource = "sheriffs"
+	case kubebindv1alpha2.NamespacedScope:
+		kind = "Cowboy"
+		resource = "cowboys"
+	default:
+		require.Fail(t, "unhandled scope %q", scope)
+	}
+	performBindingWithBrowser(t, backendAddr, providerClusterID, consumerCfg, consumerKubeconfig, resource)
+
+	t.Log("Testing resource creation and synchronization...")
+	testKCPResourceSync(t, consumerCfg, providerCfg, scope, kind, resource)
+}
+
+func testKcpClient(t testing.TB, cfg *rest.Config, scope kubebindv1alpha2.InformerScope, gvr schema.GroupVersionResource, namespace string) dynamic.ResourceInterface {
+	t.Helper()
+
+	client := framework.DynamicClient(t, cfg).Resource(gvr)
+	if scope == kubebindv1alpha2.NamespacedScope && namespace != "" {
+		return client.Namespace(namespace)
+	}
+	return client
+}
+
+func testKCPResourceSync(t *testing.T, consumerCfg, providerCfg *rest.Config, scope kubebindv1alpha2.InformerScope, kind, resource string) {
+	serviceGVR := schema.GroupVersionResource{Group: "wildwest.dev", Version: "v1alpha1", Resource: resource}
+
+	consumerClient := testKcpClient(t, consumerCfg, scope, serviceGVR, "default")
+	// provider side has a namespace with a random name for the consumer
+	// so it needs to be a non namespaced client
+	providerClient := testKcpClient(t, providerCfg, scope, serviceGVR, "")
+
+	t.Run("instance created downstream syncs upstream", func(t *testing.T) {
+		t.Logf("Creating %s instance on consumer side", kind)
+
+		resourceInstance := &unstructured.Unstructured{
+			Object: map[string]any{
+				"apiVersion": "wildwest.dev/v1alpha1",
+				"kind":       kind,
+				"metadata": map[string]any{
+					"name": "test-" + resource,
+				},
+				"spec": map[string]any{
+					"intent": "draw",
+				},
+			},
+		}
+
+		kcptestinghelpers.Eventually(t, func() (bool, string) {
+			_, err := consumerClient.Create(t.Context(), resourceInstance, metav1.CreateOptions{})
+			return err == nil, fmt.Sprintf("Error creating %s instance: %v", resource, err)
+		}, wait.ForeverTestTimeout, time.Millisecond*100)
+
+		t.Logf("Waiting for %s instance to be synced to provider side", resource)
+		var instances *unstructured.UnstructuredList
+		kcptestinghelpers.Eventually(t, func() (bool, string) {
+			var err error
+			instances, err = providerClient.List(t.Context(), metav1.ListOptions{})
+			return err == nil && len(instances.Items) >= 1, fmt.Sprintf("Error listing %s instances: %v", resource, err)
+		}, wait.ForeverTestTimeout, time.Millisecond*100)
+
+		require.Equal(t, 1, len(instances.Items), "Expected exactly one %s instance on provider side", resource)
+	})
+
+	t.Run("instance spec updated downstream syncs upstream", func(t *testing.T) {
+		t.Logf("Updating %s spec on consumer side", resource)
+
+		require.Eventually(t, func() bool {
+			obj, err := consumerClient.Get(t.Context(), "test-"+resource, metav1.GetOptions{})
+			if err != nil {
+				return false
+			}
+
+			unstructured.SetNestedField(obj.Object, "holster", "spec", "intent") //nolint:errcheck
+			_, err = consumerClient.Update(t.Context(), obj, metav1.UpdateOptions{})
+			return err == nil
+		}, wait.ForeverTestTimeout, 5*time.Second, "waiting for %s spec to be updated on consumer side", resource)
+
+		t.Logf("Waiting for %s spec update to sync to provider side", resource)
+		require.Eventually(t, func() bool {
+			instances, err := providerClient.List(t.Context(), metav1.ListOptions{})
+			if err != nil || len(instances.Items) == 0 {
+				return false
+			}
+
+			intent, found, err := unstructured.NestedString(instances.Items[0].Object, "spec", "intent")
+			return err == nil && found && intent == "holster"
+		}, wait.ForeverTestTimeout, 5*time.Second, "waiting for %s spec update to sync to provider side", resource)
+	})
+
+	t.Run("instance deleted downstream is deleted upstream", func(t *testing.T) {
+		t.Logf("Deleting %s instance on consumer side", resource)
+
+		err := consumerClient.Delete(t.Context(), "test-"+resource, metav1.DeleteOptions{})
+		require.NoError(t, err, "Failed to delete %s on consumer side", resource)
+
+		t.Logf("Waiting for %s instance to be deleted on provider side", resource)
+		require.Eventually(t, func() bool {
+			instances, err := providerClient.List(t.Context(), metav1.ListOptions{})
+			return err == nil && len(instances.Items) == 0
+		}, wait.ForeverTestTimeout, 5*time.Second, "waiting for %s instance to be deleted on provider side", resource)
+	})
 }
